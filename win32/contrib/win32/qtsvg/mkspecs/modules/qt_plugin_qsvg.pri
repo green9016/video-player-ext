a7b4ffea514ee3a1555dce92784529fff30d5bb8
osboxes.org <jinfengliu106@gmail.com>
2019-05-20 03:44:16 -0400

    - init git

---
+++ win32/contrib/win32/qtsvg/mkspecs/modules/qt_plugin_qsvg.pri
@@ -0,0 +1,2 @@
+QT_PLUGIN.qsvg.PATH = /home/osboxes/Documents/vlc-3.0/contrib/win32/qtsvg/plugins
+include(/home/osboxes/Documents/vlc-3.0/contrib/win32/qtsvg/mkspecs/modules-inst/qt_plugin_qsvg.pri)
---
+++ win32/contrib/win32/qtsvg/src/svg/qdoc_wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+QT_VERSION=5.6.3
+export QT_VERSION
+QT_VER=5.6
+export QT_VER
+QT_VERSION_TAG=563
+export QT_VERSION_TAG
+QT_INSTALL_DOCS=/home/osboxes/Documents/vlc-3.0/contrib/i686-w64-mingw32/doc
+export QT_INSTALL_DOCS
+exec /home/osboxes/Documents/vlc-3.0/contrib/i686-w64-mingw32/bin/qdoc "$@"
